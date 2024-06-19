f23fda4be3b6676af1483298dd4e9683f356578b
NiclBro301 <nickbro1234789@gmail.com>
2024-06-19 18:47:50 +0300

    new structure of project, BASE

--- index.js
+++ index.js
@@ -1,102 +1,29 @@
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
-const hbs = require('hbs');
-const path = require('path');
-// const User = require('./models/user');
-// const sequelize = require('./models/connector');
+// npm init -y
+// npm install express
+// node index.js
+// npm install hbs
+// npm install nodemon
+// "start": "nodemon index.js" (внутри package.json в строке scripts)
+// npm install @faker-js/faker
+// npm start
 
-const app = express();
+let express = require(`express`)
+let app = express()
 
-app.use(session({ secret: '13AF@$FC@$@@', resave: false, saveUninitialized: false }));
+app.use(express.static(`public`))
 
+const hbs = require('hbs')
+app.set('views', 'views')
+app.set('view engine', 'hbs')
 
-passport.use(new LocalStrategy(async (username, password, done) => {
-  try {
-    const user = await User.findOne({ where: { username } });
-    if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return done(null, false, { message: 'Incorrect password.' });
-    }
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-}));
+let bodyParser = require(`body-parser`)
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+let port = 3000
+app.listen(port, function(){
+    console.log(`http://localhost:${port}/`)
+})
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findByPk(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
-
-
-app.use(express.urlencoded({ extended: false }));
-
-
-app.get('/', (req, res) => {
-  res.render('index', { user: req.user });
-});
-
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-}));
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  try {
-    await User.create({ username, password: hashedPassword });
-    res.redirect('/login');
-  } catch (err) {
-    res.redirect('/register');
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-
-
-// ВНИМАНИЕ! ВНИМАНИЕ! Мне было лень в докере поднимать БД, но я уверен что оно работать будет, поэтому закоментил
-// Мне лениться можно, а вам - НЕТ, РАССКОМЕНТИ И ДЕЛАЙ КАК НАДО!!!
-// sequelize.sync()
-//   .then(() => {
-//     app.listen(3000, () => {
-//       console.log('Server is running on port 3000');
-//     });
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-// Не говори, что мне делать, я сам всё сделаю как надо! -_-
+app.get(`/`, function(req, res){    
+  res.render(`index`)
+})

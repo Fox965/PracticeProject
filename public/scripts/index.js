ebfbc36696fadc62a2cab7e93dd00f6b0bcf1a0d
Kostess <qaz1234567890838@gmail.com>
2024-07-01 22:34:27 +0300

    главная страница готова

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -21,6 +21,55 @@ document.querySelector(`#next`).addEventListener(`click`, () => {
 })
 // END_SLIDER
 
+// ANIMATION_MAIN_PAGE
+let moving = 0
+function handleScroll() {
+    const messages = document.querySelectorAll('.message');
+
+    messages.forEach((message, index) => {
+        const rectMessage = message.getBoundingClientRect();
+        const isVisibleMessage = rectMessage.top < window.innerHeight && rectMessage.bottom >= 0;
+
+        if (isVisibleMessage && !message.classList.contains('visible')) {
+            setTimeout(() => {
+                message.classList.add('visible');
+            }, index * 700);
+        }
+    });
+
+    const moveableElements = document.querySelectorAll('.back-image');
+    moveableElements.forEach(element => {
+        const startTop = parseInt(element.getAttribute('data-start-top'));
+        const startRight = parseInt(element.getAttribute('data-start-right'));
+        const startLeft = parseInt(element.getAttribute('data-start-left'));
+
+        const rectBackImage = element.getBoundingClientRect();
+        const isVisibleBackImage = rectBackImage.top < window.innerHeight && rectBackImage.bottom >= 0;
+
+        if (isVisibleBackImage) {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            const newTop = startTop + scrollTop * 0.5; // Изменяйте множитель для скорости движения
+            const newRight = startRight - scrollTop * 0.3; // Изменяйте множитель для скорости движения
+            const newLeft = startLeft - scrollTop * 0.3; // Изменяйте множитель для скорости движения
+
+            if (startTop !== undefined) {
+                element.style.top = `${newTop}px`;
+            }
+            if (startRight !== undefined) {
+                element.style.right = `${newRight}px`;
+            }
+            if (startLeft !== undefined) {
+                element.style.left = `${newLeft}px`;
+            }
+        }
+
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
+// END_ANIMATION_MAIN_PAGE
+
 let enterForm = document.querySelector(`#enterForm`);
 let regForm = document.querySelector(`#regForm`);
 let enterButton = document.querySelector(`#enterButton`);
